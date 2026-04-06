8ad6a2a788eae15020b054cb37f57afbd9cc0952
Allen923 <ncha1999@gmail.com>
2026-04-06 21:38:18 +0800

    '清洗資料、編排columns'

--- 抓取台灣銀行每日行情.py
+++ 抓取台灣銀行每日行情.py
@@ -3,4 +3,24 @@ import pandas as pd
 url = "https://rate.bot.com.tw/xrt/all/2026-04-02"
 
 df = pd.read_html(url)[0]
+df = df.iloc[:, :5]
+df.columns = ["幣別", "現金買入", "現金賣出", "即期買入", "即期賣出"]
+df.insert(0, "幣別名稱", df["幣別"].str.split().str[0])
+df.insert(1, "幣別代號", df["幣別"].str.split().str[1].str.strip("()"))
+df["日期"] = "2026-04-02"
+df.drop("幣別", axis=1, inplace=True)
+columns_list = [
+    "日期",
+    "幣別名稱",
+    "幣別代號",
+    "現金買入",
+    "現金賣出",
+    "即期買入",
+    "即期賣出",
+]
+df = df[columns_list]
+for i in df.columns[3:]:
+    df[i] = pd.to_numeric(df[i], errors="coerce")
+
+
 print(df.head())
